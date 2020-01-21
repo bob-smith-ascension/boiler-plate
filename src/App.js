e1bbda3957e92a17bced3e69cbad440c62f55682
David <david.shams@gmail.com>
2020-01-21 12:36:18 -0600

    props.children

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import List from './components/list/List';
 
 function App() {
   return (
     <>
     <h3>React</h3>
+    <List>
+      <button>A</button>
+      <button>B</button>
+      <button>C</button>
+    </List>
     </>
   );
 }
---
+++ src/components/list/List.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const List = (props) => {
+    return (
+        <div>
+            {props.children}
+            {props.children}
+        </div>
+    );
+}
+
+export default List;
